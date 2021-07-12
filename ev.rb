a5b41487bcff26d46505fc70b8f43c3357f4e21c
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-12 23:01:59 +0900

    Add files via upload

---
+++ ev.rb
@@ -0,0 +1,25 @@
+﻿def fizz(*)
+  "fizz"
+end
+
+def buzz(*)
+  str = "#{yield} " if block_given?
+  "#{str}buzz"
+end
+
+def other(num)
+  num
+end
+
+def fizz_buzz(num)
+	str = 'fizz'      if num % 3 == 0
+	str = 'buzz'      if num % 5 == 0
+	str = 'buzz{ fizz }' if num % 15 == 0
+	str ||= num.to_s
+  eval("p #{str}")
+end
+
+for i in 1..100 do
+  fizz_buzz(i)
+end
+
